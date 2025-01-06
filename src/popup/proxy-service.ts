f479c0ae21c7363508354fd59eb8790dd6384c06
dzhisl <islmsc101@gmail.com>
2025-01-06 17:42:01 +0300

    add delay before setting proxy

--- src/popup/proxy-service.ts
+++ src/popup/proxy-service.ts
@@ -18,24 +18,33 @@ export function getProxyByPromo(promoCode: string): Promise<void> {
       });
   }
   
-  // Function to set proxy configuration
-  export function setProxyConfig(host: string, port: number, username?: string, password?: string): void {
-    const config = {
-      mode: "fixed_servers",
-      rules: {
-        proxyForHttps: { host, port },
-        bypassList: []
-      }
-    };
-  
-    console.log(config);
-  
-    if (username && password) {
-      sendCredentialsToBackground(username, password);
-    }
-  
-    chrome.proxy.settings.set({ value: config, scope: "regular" });
+// Utility function to sleep for a given time
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Function to set proxy configuration
+export async function setProxyConfig(host: string, port: number, username?: string, password?: string): Promise<void> {
+  const config = {
+    mode: "fixed_servers",
+    rules: {
+      proxyForHttps: { host, port },
+      bypassList: [],
+    },
+  };
+
+  console.log(config);
+
+  if (username && password) {
+    sendCredentialsToBackground(username, password);
+    console.log("Sleeping before sending creds to background")
+    await sleep(500); // Add a 500ms delay
+    console.log("Sleep done")
   }
+
+  chrome.proxy.settings.set({ value: config, scope: "regular" });
+  console.log("Set proxy in chrome settings")
+}
   
   // Function to send credentials to the background script
   export function sendCredentialsToBackground(username: string, password: string): void {
